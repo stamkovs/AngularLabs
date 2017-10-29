8deae33825c643b4c6781e489d47fa6b4b2ca482
Stojan Stamkov <sst@nix.mk>
2017-10-29 17:51:31 +0100

    Added igrac model, igrac.service, and imported it in app.component but it wont work

--- ngLab1/src/app/app.component.ts
+++ ngLab1/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {IgracService} from '../app/services/Igrac.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ ngLab1/src/app/services/Igrac.service.ts
@@ -0,0 +1,50 @@
+/**
+ * Created by Stole on 29.10.2017.
+ */
+
+import {Injectable} from '@angular/core';
+import {Igrac} from '../../app/model/igrac';
+
+@Injectable()
+export class IgracService {
+
+    constructor() {
+    }
+
+    private idSequence = 0;
+
+    private igraci = [{
+            id: 1,
+            ime: "Blake",
+            prezime: "Griffin",
+            tim: "Los Angeles Clippers",
+            pozicija: "Power Forward",
+            plata: 100000
+        },
+        {
+            id: 2,
+            ime: "Deandre",
+            prezime: "Jordan",
+            tim: "Los Angeles Clippers",
+            pozicija: "Center",
+            plata: 50000
+        },
+        {
+            id: 3,
+            ime: "Milos",
+            prezime: "Teodosic",
+            tim: "Los Angeles Clippers",
+            pozicija: "Point Guard",
+            plata: 20000
+        },
+        {
+            id: 4,
+            ime: "Patrick",
+            prezime: "Beverly",
+            tim: "Los Angeles Clippers",
+            pozicija: "Point Guard",
+            plata: 40000
+        }
+    ];
+
+}
